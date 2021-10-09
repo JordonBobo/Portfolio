f669dfd88a0196f7db3b50e6d3fe17c39338d6e0
Jordon Bobo <jordon.bobo@outlook.com>
2021-10-09 16:32:47 -0600

    brought in old content, still rough

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';   
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
   return (
-    <div>Test</div>
+    <Router>
+      <div>
+      <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/ReactPortfolio/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+      <Footer />
+      </div>
+    </Router>
   );
 }
 
