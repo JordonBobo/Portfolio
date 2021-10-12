60dd60f3a171009822cd268e0f3f155f64f03b8d
Jordon Bobo <jordon.bobo@outlook.com>
2021-10-12 12:34:09 -0600

    updated about me section

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,41 +12,76 @@ function About() {
       <div className="container profile">
         <div className="profile left col-sm-6">
           <img src="https://live.staticflickr.com/65535/51207744275_469179bc0b_b.jpg" 
-          alt="Jordan in Yanghuo China, 2018"
+          alt="Jordan in Yangshuo China"
           className="fullWidth"/>
         </div>
       </div>
       
       <div className="container myFont">
         <p>
-My name is LX "Jordon" Bobo. 
-Welcome to my portfolio. I encourage anyone reading 
-this to feel free to reach out to me if you have any questions or are interested in anything 
-you see in my portfolio. This page is devoted to my transition to becoming a software 
-developer or data engineer. <br /> <br />
+LX "Jordon" Bobo: Super Geek, turned Super Student, and now back to Geek. <br /> <br />
 
-Although I have had a lifelong interest in computers and coding, and even built my first 
-computer at age 10, my experiences in high school and college encouraged me to pursue my 
-other passions such as history, agriculture, and linguistics. After getting my bachelors 
-degree from Utah State University, I initially attempted to pursue a career in archiving 
-and library science, and spent nearly 4 years working at the Murray Library. While I was 
-there, I helped facilitate the procurement and deployment of a new Integrated Library System. 
-That project rekindled my appreciation for software development and I decided to pursue 
-coding and analytics. <br /> <br />
+My interest in computers and coding started early. I built my first computer at age 11 
+and my first website at age 14. In high school I was what you might call a super geek. I 
+used a ram chip as a keychain. I carried a palm pilot in the days before cell phones 
+became computers. I even had a genuine pocket protector with my school's logo on it. 
+<br /> <br />
 
-Until recently I worked as a Systems Support Specialist for the Utah Higher Education Assistance Authority, 
-which is part of the Utah System of Higher Education. While there, I worked with some pretty 
-great people to help make sure that education was available to anyone and everyone. I worked 
-mostly behind the scenes monitoring and troubleshooting software/computer scripts that 
-were developed in-house, granting and revoking access to various databases, programs, and 
-networks, as well as assisting employees and clients with our company’s software. <br /> <br />
+As I transitioned to college, my interests shifted to learning and education as a whole. 
+I found myself studying a bit of everything. Economics, sociology, linguistics, history, 
+and even agriculture, just to name a few. My passion became learning and exploring new 
+ideas and concepts. I maintain this appreciation for new ideas and paradigms to this day. 
+<br /> <br />
 
-In order to improve my skills and continue to pivot my career toward software and coding, I 
-am currently engaged in the University of Utah’s &nbsp;
+With a bachelors degree that included 3 minors and more than 200 credits, I entered the 
+professional workforce and spent nearly 4 years trying to get a square peg to fit into 
+a round hole. I loved the idea of working at a library, but my skills and abilities were 
+being underutilized and I had a lot more to offer than I could ever give in that 
+environment. <br /> <br />
+
+I took inspiration for a career change when I helped the library acquire and implement 
+an entirely new software platform and website. I also helped decide which updates 
+needed to take priority as the software evolved to meet the needs of our customers 
+and staff. Learning about the lifecycle of the different modules and how small changes 
+could have such a significant impact on the user experience rekindled an interest in 
+coding and software development. <br /> <br />
+
+I soon after left the library in search of something more appropriate to my skills 
+and abilities. At the Utah Higher Education Assistance Authority (UHEAA) I was able to 
+show off my ability to adapt to a frequently changing environment and solve
+complex problems. I eventually made my way into the Technology and Innovation department 
+as a Systems Support Specialist. My job was to work with programmers, analysts, clients, 
+and call center management to developed and maintained new programs and applications to 
+meet the rapidly changing needs of the company and our clients. <br /> <br />
+
+My passion for the work encouraged me to continue to develop my skills so I could be 
+a more valuable asset to my team and to my own career. Although very well rounded in my 
+abilities, my coding skills were mostly self taught and out of date. I knew coding would 
+have the most value to my long term goals and last fall I enrolled in the University of Utah’s &nbsp;
 <a href="https://continue.utah.edu/proed/academy/certificate/web-coding-bootcamp" target="_blank" rel="noreferrer" className="red"> 
-Coding BootCamp</a> &nbsp; in order to advance my skills as a developer. Currently I am focusing on 
-learning full-stack in order to better understand the entire scope of software development. In the 
-future however, I plan on focusing more on back-end programming, data analystics, and data science. <br />
+Coding BootCamp</a> &nbsp; <br /> <br /> to significantly improve my coding skills.
+
+My intention was to become a more valuable member of my team and 
+to use those skills to take on additional responsibilities and advance my position 
+at UHEAA. Unfortunately the company faced difficult times and approximately half of their 
+staff was laid off earlier this year, myself included. I continued with the Coding Bootcamp 
+and graduated in June of 2021 with a Certificate in Full Stack Web Development. <br /> <br />
+
+Recently my coding efforts have been focused on making a videogame called "Homesteader".
+The game has been a slow process and I do not currently have a deployed version. The game 
+uses modern tools and languages such as React, JavaScript, and Bootstrap to engage the 
+player in decision making and prioritization to achieve goals to improve their homestead. 
+It takes inspiration from vintage interactive fiction games such as Zork, as well as modern 
+text based games such as A Dark Room by Doublespeak Games, or Kittens by Dark Souls. 
+<br /> <br />
+
+As I continue to develop and improve my skills, my goal is to utilize my diverse experience 
+and education toward every project I work on and every team I work with. I am currently looking 
+for new opportunities and am open to working for any company or organization that focuses on 
+collaboration, innovation, and the improvement of our community. 
+
+
+ <br />
         </p>
         
       </div>
